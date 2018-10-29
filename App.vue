f14da7e30b8e0386f12f6843326d0532b80c69fe
Bogdan Luca <luca.bogdan@gmail.com>
2018-10-29 11:49:00 +0200

    Initial commit

---
+++ App.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="app">
+    Handler:
+    <select v-model="handlerId">
+      <option value="1">1</option>
+      <option value="2">2</option>
+    </select><br><br>
+    <button @click="clickHandler">clik me!</button>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      handlerId: '1',
+      clickHandler: this.handler1
+    }
+  },
+  methods: {
+    handler1() {
+      console.log('handler 1');
+    },
+    handler2() {
+      console.log('handler 2');
+    }
+  },
+  watch: {
+    handlerId(value) {
+      this.clickHandler = this[`handler${value}`];
+    }
+  }
+};
+</script>
+
+<style>
+body,
+input {
+  font-family: "Exo 2", sans-serif;
+  font-size: 16px;
+}
+</style>
